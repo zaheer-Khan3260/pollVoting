d81248e5c98688ed42a9ac332cd471fac4e2eb34
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-12-06 12:43:09 +0530

    add routes to get leaderboard

--- backend/src/routes/leaderboard.routes.js
+++ backend/src/routes/leaderboard.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { getLeaderboard } from "../controllers/leaderboard.controller.js";
 
 const router = Router()
 
-
+router.route('/').get(getLeaderboard);
 
 export default router;
